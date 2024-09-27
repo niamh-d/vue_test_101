558deae9a2b9b0726aa03cc47007459e8c3a6a1c
Niamh <niamh.linguist@gmail.com>
2024-09-27 18:06:30 +0300

    testing simple email ipout

--- src/components/__tests__/Form.spec.js
+++ src/components/__tests__/Form.spec.js
@@ -1,3 +1,12 @@
 import { expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
 import Form from '../Form.vue'
+
+it('sets the value of email', async () => {
+  const wrapper = mount(Form)
+  const emailInput = wrapper.find('input')
+
+  await emailInput.setValue('test@example.com')
+
+  expect(emailInput.element.value).toBe('test@example.com')
+})
